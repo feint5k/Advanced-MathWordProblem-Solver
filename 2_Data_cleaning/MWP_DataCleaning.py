3f5b08f3d8d0bf36446c2b82e4e5d5e3c55c1141
feint5k <feint5k@proton.me>
2023-08-26 12:37:47 +0000

    Fix windows compilation

--- 2_Data_cleaning/MWP_DataCleaning.py
+++ 2_Data_cleaning/MWP_DataCleaning.py
@@ -6,4 +6,7 @@ import nltk
 from word2number import w2n
 from nltk.stem.snowball import SnowballStemmer
 
-df = pd.read_csv('./Cleaned Data/trai
+df = pd.read_csv('./Cleaned Data/trainData_univariable.csv')
+
+df = df[np.invert(np.array(df['text'].isna()))]
+numMap = {"twice": 2, "double": 2, "thrice": 3, "half": "1/2", "tenth": "1/10", "quarter": "1/4", "fifth":
