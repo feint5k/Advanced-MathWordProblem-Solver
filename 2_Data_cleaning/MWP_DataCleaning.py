69b876edcf3cf3a32dc20abf386525f04fcf9ccf
feint5k <feint5k@proton.me>
2023-08-26 15:21:45 +0000

    crypto: crc32c - add missing crypto module alias

--- 2_Data_cleaning/MWP_DataCleaning.py
+++ 2_Data_cleaning/MWP_DataCleaning.py
@@ -9,4 +9,6 @@ from nltk.stem.snowball import SnowballStemmer
 df = pd.read_csv('./Cleaned Data/trainData_univariable.csv')
 
 df = df[np.invert(np.array(df['text'].isna()))]
-numMap = {"twice": 2, "double": 2, "thrice": 3, "half": "1/2", "tenth": "1/10", "quarter": "1/4", "fifth":
+numMap = {"twice": 2, "double": 2, "thrice": 3, "half": "1/2", "tenth": "1/10", "quarter": "1/4", "fifth": "1/5"}
+fraction = {"third": "/3", "half": "/2", "fourth": "/4", "sixth": "/6", "fifth": "/5", "seventh": "/7", "eighth": "/8",
+            "ninth": "/9", "tenth": "/10"
