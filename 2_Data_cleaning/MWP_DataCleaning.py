98895902f6bbf4a89a970f4483f9b0dffe63574a
feint5k <feint5k@proton.me>
2023-08-28 05:30:24 +0000

    fixed compile error

--- 2_Data_cleaning/MWP_DataCleaning.py
+++ 2_Data_cleaning/MWP_DataCleaning.py
@@ -12,4 +12,11 @@ df = df[np.invert(np.array(df['text'].isna()))]
 numMap = {"twice": 2, "double": 2, "thrice": 3, "half": "1/2", "tenth": "1/10", "quarter": "1/4", "fifth": "1/5"}
 fraction = {"third": "/3", "half": "/2", "fourth": "/4", "sixth": "/6", "fifth": "/5", "seventh": "/7", "eighth": "/8",
             "ninth": "/9", "tenth": "/10", "eleventh": "/11", "twelfth": "/12", "thirteenth": "/13",
-            "fourteenth": "/14", "fifteenth": "/15", "sixteenth": "/16", "seventeenth": "/17", "eighteenth": "/1
+            "fourteenth": "/14", "fifteenth": "/15", "sixteenth": "/16", "seventeenth": "/17", "eighteenth": "/18",
+            "nineteenth": "/19", "twentieth": "/20", "%": "/100"}
+
+stemmer = SnowballStemmer(language='english')
+
+
+# Convert fractions to floating point numbers
+def convert_to_fl
